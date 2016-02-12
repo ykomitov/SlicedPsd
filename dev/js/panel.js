93c19ae073cfebc6d9bf3c5aebf51d827eaee63f
ykomitov <yavork@gmail.com>
2016-02-12 16:56:49 +0200

    fixed path for background textures

--- dev/js/panel.js
+++ dev/js/panel.js
@@ -3,9 +3,9 @@
     // Check if there is a key for background in local storage
     var currentBackground = localStorage.getItem("background");
     if (currentBackground) {
-      $('body').css('background-image', 'url("../images/bg-textures/' + currentBackground + '")');
+      $('body').css('background-image', 'url("images/bg-textures/' + currentBackground + '")');
     }else{
-      $('body').css('background-image', 'url("../images/bg-textures/t-5.jpg")');
+      $('body').css('background-image', 'url("images/bg-textures/t-5.jpg")');
     }
 
     // Open the lateral panel
@@ -30,7 +30,7 @@
       var textureId = i + 1;
       $currentDiv.addClass('bg-texture');
       $currentDiv.attr('id', 't-' + textureId);
-      $currentDiv.css('background-image', 'url("../images/bg-textures/t-' + textureId + '.jpg")');
+      $currentDiv.css('background-image', 'url("images/bg-textures/t-' + textureId + '.jpg")');
 
       if (i > 0 && (i + 1) % 4 == 0) {
         $currentDiv.addClass('end');
@@ -43,7 +43,7 @@
     $('.bg-texture').on('click', function (event) {
       event.preventDefault();
       var textureName = event.target.id + '.jpg';
-      $('body').css('background-image', 'url("../images/bg-textures/' + textureName + '")');
+      $('body').css('background-image', 'url("images/bg-textures/' + textureName + '")');
       localStorage.setItem("background", textureName);
     });
   });
