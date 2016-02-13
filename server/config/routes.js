1f39f26acb3c17007de5c305b7de6ec6fd697e22
ykomitov <yavork@gmail.com>
2016-02-13 09:00:04 +0200

    Added dummy pages

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,11 +4,31 @@ module.exports = function (app) {
     res.render('index');
   });
 
-  app.get('/home.html', function(req, res) {
-    res.render('home');
+  app.get('/about', function(req, res) {
+    res.render('about');
   });
 
-  app.get('/post.html', function(req, res) {
+  app.get('/post', function(req, res) {
     res.render('post');
+  });
+
+  app.get('/blog', function(req, res) {
+    res.render('blog');
+  });
+
+  app.get('/contact', function(req, res) {
+    res.render('contact');
+  });
+
+  app.get('/shortcodes', function(req, res) {
+    res.render('shortcodes');
+  });
+
+  app.get('/templates', function(req, res) {
+    res.render('templates');
+  });
+
+  app.get('*', function (req, res) {
+    res.render('error');
   });
 };
